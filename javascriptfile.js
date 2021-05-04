622469825a0816240d28a2c3f9dd997f87df48a8
Neskovic <stefan.neskovic@elev.ga.ntig.se>
2021-05-04 12:30:10 +0200

    jobbat med formulär sida

--- javascriptfile.js
+++ javascriptfile.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student på NTI Gymnasiet I Södertälje"],
+        strings: ["Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student på NTI Gymnasiet I Södertälje"],
+        strings: ["Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
